a2664fbd888def69ab51708a669e749be89a2840
mirarosovsky <mirarosovsky@localhost>
2024-01-24 15:40:47 -0600

    Studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,17 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("hey loser! put in a number");
+		int n1 = in.nextInt();
+		System.out.println("reallyyyy, you chose that number? put in another one.");
+		double n2 = in.nextInt();
+		System.out.println("im gonna average those hehe " + (n1 + n2)/2);
+	}
+
+}
--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,10 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+boolean is0rdered = (x>y && y>z)||(z>y && y>x);
+ System.out.print("True or False? " + is0rdered);
+
+
 
 	}
 
